8d727099e8949694c2cd33dd2ea9c8dcf841ae73
Matt Miranda <mattmiranda55@gmail.com>
2025-11-10 22:32:24 -0500

    setup layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
-import NebulaLayout from './components/NebulaLayout.vue'
+import NebulaLayout from './components/layout/NebulaLayout.vue';
+import NebulaEditor from './components/ui/NebulaEditor.vue';
+import NebulaHeader from './components/ui/NebulaHeader.vue';
+import NebulaSidebar from './components/ui/NebulaSidebar.vue';
+import NebulaTerminal from './components/ui/NebulaTerminal.vue';
 </script>
 
 <template>
-  <NebulaLayout />
+  <NebulaLayout>
+
+    <template #sidebar>
+      <NebulaSidebar />
+    </template>
+
+    <template #header>
+      <NebulaHeader />
+    </template>
+
+    <template #editor>
+      <NebulaEditor />
+    </template>
+
+    <template #terminal>
+      <NebulaTerminal />
+    </template>
+
+  </NebulaLayout>
 </template> 
 
 <style scoped>
